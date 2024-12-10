5fc0af835db0e5977802a4fc531b636beef11a51
yjydist <yjydist@gmail.com>
2024-12-10 20:48:28 +0800

    init

---
+++ src/test/java/com/yjydist/ecommercejava/ECommerceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjydist.ecommercejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
